87d3ad538b3a9125613ff4a040ef285bf27c34bf
NoNotCar <13haslat@gmail.com>
2015-12-08 22:08:36 +0000

    PINK

--- Worlds.py
+++ Worlds.py
@@ -17,8 +17,10 @@ worlds = [World([Img.img2("Grass"), Img.img2("Block"), Img.img2("Grass2")], "Cum
           World([Img.img2("Sand"), Img.img2("Block"), Img.img2("CastleInGrey"), Img.img2("Oil")], "Desert",
                 (181, 117, 0), (250, 250, 0)),
           World([Img.img2("Spacefloor"), Img.img2("CastleBlock"), Img.img2("PuzzBlock"),Img.img2("IceSpace")], "ChOrDs", (255, 255, 255),
-                (255, 81, 226)),
+                (255, 255, 255)),
           World([Img.img2("RockSnow"), Img.img2("RedBlock"), Img.img2("CastleIn"), Img.img2("RockIce")], "W5V",
-                (255, 93, 0), (200, 59, 0))]
+                (255, 93, 0), (200, 59, 0)),
+          World([Img.img2("PinkGround"), Img.img2("PinkBlock"), Img.img2("Pink2"), Img.img2("PinkIce")], "DOMINIK_4", (255, 186, 245),
+                (255, 81, 226))]
 castle = World([Img.img2("CastleFloor"), Img.img2("CastleBlock"), Img.img2("CastleIn")], "Flags", (125, 125, 125),
                (200, 200, 200))
